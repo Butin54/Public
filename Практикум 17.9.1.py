25da3afb3cc6cbb94d94d8a73c814a13a811abd7
Butin54 <110367746+Butin54@users.noreply.github.com>
2022-10-06 11:33:57 +0700

    Add files via upload

---
+++ Практикум 17.9.1.py
@@ -0,0 +1,45 @@
+import random
+def quicksort_random(array, left, right):    
+    p = random.choice(array[left:right+1])
+    i,j = left, right
+    while i <= j:
+        while array[i] < p:
+            i += 1
+        while array[j] > p:
+            j -= 1
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+        if j > left:
+            quicksort_random(array, left, j)
+    if right > i:
+        quicksort_random(array, i, right)
+    return array
+def binary_search(array, element, left, right): 
+    if left > right:
+        return "В введенной последовательности нет элементов меньше заданного числа"
+    middle = (right+left) // 2
+    if array[middle] >= element and array[middle-1] < element:
+        return middle-1
+    elif element < array[middle]:
+        return binary_search(array, element, left, middle-1)
+    else:
+        return binary_search(array, element, middle+1, right)
+array_number = []
+while not array_number:
+    try:
+        array_number = list(map(int,
+                        		input("Введите последовательность целых чисел через пробел").split()))
+    except ValueError as e:
+        print("Вы ввели последовательность чисел не корректно")
+array_number = quicksort_random(array_number, 0, len(array_number)-1)
+x = None
+while not isinstance(x, int):
+    try:
+        x = int(input("Задайте целое число"))
+    except ValueError as e:
+        print("Ввод некорректен")
+print(array_number)
+print(x)
+print(binary_search(array_number, x, 0, len(array_number)-1))
